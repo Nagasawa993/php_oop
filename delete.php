3cecaa739312456c8132333ede6683014a5717e6
Nagasawa Yoshitoki <>
2019-10-31 10:29:50 +0800

    10/29授業終了分

---
+++ delete.php
@@ -0,0 +1,30 @@
+<?php
+
+var_dump($_GET['id']);
+
+require_once('Models/Todo.php');
+
+$id = $_GET['id'];
+
+$todo = New Todo;
+
+$todo->delete($id);
+
+header('Location: index.php');
+
+
+
+
+
+// require_once('Models/Todo.php');
+
+// $id = $_GET['id'];
+
+// //Todoクラスをインスタンス化
+// $todo = New Todo;
+
+// //Todoクラスのcreateメソッドを実行
+// $todo->delete($id);
+
+// //index.phpに戻る
+// header('Location: index.php');
